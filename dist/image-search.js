63f1e66c2661c0b2b023de9089bcb454f2dd2183
Tim Davis <thebearingedge@gmail.com>
2016-05-03 22:07:03 -0700

    learn google parameters

--- dist/image-search.js
+++ dist/image-search.js
@@ -4,45 +4,59 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
-var _googleImages = require('google-images');
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
 
-var _googleImages2 = _interopRequireDefault(_googleImages);
+var _url = require('url');
 
 var _expressAsyncWrap = require('express-async-wrap');
 
 var _expressAsyncWrap2 = _interopRequireDefault(_expressAsyncWrap);
+
+var _requestPromise = require('request-promise');
+
+var _requestPromise2 = _interopRequireDefault(_requestPromise);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { return step("next", value); }, function (err) { return step("throw", err); }); } } return step("next"); }); }; }
 
 var _process$env = process.env;
-var CSE_ID = _process$env.CSE_ID;
-var CSE_KEY = _process$env.CSE_KEY;
+var key = _process$env.CSE_KEY;
+var cx = _process$env.CSE_ID;
 
+var searchType = 'image';
+var safe = 'medium';
+var num = 10;
 
-var api = (0, _googleImages2.default)(CSE_ID, CSE_KEY);
+var parameters = { key: key, cx: cx, searchType: searchType, safe: safe, num: num };
+
+var protocol = 'https';
+var host = 'www.googleapis.com';
+var pathname = '/customsearch/v1';
 
 var imageSearch = (0, _expressAsyncWrap2.default)(function () {
   var ref = _asyncToGenerator(regeneratorRuntime.mark(function _callee(_ref, res) {
     var params = _ref.params;
-    var query = _ref.query;
-    var term, images;
+
+    var q, query, uri, _ref2, images;
+
     return regeneratorRuntime.wrap(function _callee$(_context) {
       while (1) {
         switch (_context.prev = _context.next) {
           case 0:
-            term = params.term;
-            _context.next = 3;
-            return api.search(term);
-
-          case 3:
-            images = _context.sent;
-
-
-            res.json(images);
+            q = params.term;
+            query = _extends({}, parameters, { q: q });
+            uri = (0, _url.format)({ protocol: protocol, host: host, pathname: pathname, query: query });
+            _context.next = 5;
+            return (0, _requestPromise2.default)({ uri: uri, json: true });
 
           case 5:
+            _ref2 = _context.sent;
+            images = _ref2.items;
+
+            res.json(images.map(formatImageData));
+
+          case 8:
           case 'end':
             return _context.stop();
         }
@@ -54,5 +68,17 @@ var imageSearch = (0, _expressAsyncWrap2.default)(function () {
     return ref.apply(this, arguments);
   };
 }());
+
+var formatImageData = function formatImageData(_ref3) {
+  var image = _ref3.image;
+  var snippet = _ref3.snippet;
+  var link = _ref3.link;
+  return {
+    url: link,
+    snippet: snippet,
+    thumbnail: image.thumbnailLink,
+    context: image.contextLink
+  };
+};
 
 exports.default = imageSearch;
--- src/image-search.js
+++ src/image-search.js
@@ -1,21 +1,37 @@
 
-import googleImages from 'google-images'
+import { format } from 'url'
 import wrap from 'express-async-wrap'
+import request from 'request-promise'
 
 
-const { CSE_ID, CSE_KEY } = process.env
+const { CSE_KEY: key, CSE_ID: cx } = process.env
+const searchType = 'image'
+const safe = 'medium'
+const num = 10
+
+const parameters = { key, cx, searchType, safe, num }
+
+const protocol = 'https'
+const host = 'www.googleapis.com'
+const pathname = '/customsearch/v1'
 
 
-const api = googleImages(CSE_ID, CSE_KEY)
+const imageSearch = wrap(async ({ params }, res) => {
+
+  const { term: q } = params
+  const query = { ...parameters, q }
+  const uri = format({ protocol, host, pathname, query })
+
+  const { items: images } = await request({ uri, json: true })
+  res.json(images.map(formatImageData))
+})
 
 
-const imageSearch = wrap(async ({ params, query }, res) => {
-
-  const { term } = params
-
-  const images = await api.search(term)
-
-  res.json(images)
+const formatImageData = ({ image, snippet, link }) => ({
+  url: link,
+  snippet,
+  thumbnail: image.thumbnailLink,
+  context: image.contextLink
 })
 
 
